9fbb32c7f7ad0c71bd0f0ef09de0680abbc5138b
Max Cavinger <max.cavinger@gmail.com>
2014-05-20 16:06:03 +0200

    fix marketbts n menu

--- app/js/app.js
+++ app/js/app.js
@@ -13,9 +13,9 @@ config(function ($locationProvider) {
  $locationProvider.html5Mode(true);
 }).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.when('/line/:coinValue', {templateUrl: 'partials/line.html', controller: 'lineCtrl'});
   $routeProvider.when('/ticker', {templateUrl: 'partials/ticker.html', controller: 'tickerCtrl'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/ticker'});
 }]);
+
+
---
+++ app/js/jqmods.js
@@ -0,0 +1,6 @@
+$('body').on('click', '.btn-group button', function (e) {
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+    
+    //do any other button related things
+});
