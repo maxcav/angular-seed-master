16c1fb6195bbba625d5338b9ee8b3459261bedb8
RiskyRagnar <mattiasnoren89@gmail.com>
2014-06-02 12:32:22 +0200

    hejsn

--- server/wiki.php
+++ server/wiki.php
@@ -4,22 +4,25 @@
 // format=json: in json format
 // prop=text: send the text content of the article
 // section=0: top content of the page
+function setWiki($coinValue) {
+	$url = 'http://en.wikipedia.org/w/api.php?action=parse&page=peercoin&format=json&prop=text&section=0';
+	$ch = curl_init($url);
+	curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt ($ch, CURLOPT_USERAGENT, "TestScript"); // required by wikipedia.org server; use YOUR user agent with YOUR contact information. (otherwise your IP might get blocked)
+	$c = curl_exec($ch);
 
-$url = 'http://en.wikipedia.org/w/api.php?action=parse&page=peercoin&format=json&prop=text&section=0';
-$ch = curl_init($url);
-curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt ($ch, CURLOPT_USERAGENT, "TestScript"); // required by wikipedia.org server; use YOUR user agent with YOUR contact information. (otherwise your IP might get blocked)
-$c = curl_exec($ch);
+	$json = json_decode($c);
 
-$json = json_decode($c);
+	$content = $json->{'parse'}->{'text'}->{'*'}; // get the main text content of the query (it's parsed HTML)
 
-$content = $json->{'parse'}->{'text'}->{'*'}; // get the main text content of the query (it's parsed HTML)
+	// pattern for first match of a paragraph
+	$pattern = '#<p>(.*)</p>#Us'; // http://www.phpbuilder.com/board/showthread.php?t=10352690
+	if(preg_match($pattern, $content, $matches))
+	{
+	    // print $matches[0]; // content of the first paragraph (including wrapping <p> tag)
+	    print strip_tags($matches[1]); // Content of the first paragraph without the HTML tags.
+	}
 
-// pattern for first match of a paragraph
-$pattern = '#<p>(.*)</p>#Us'; // http://www.phpbuilder.com/board/showthread.php?t=10352690
-if(preg_match($pattern, $content, $matches))
-{
-    // print $matches[0]; // content of the first paragraph (including wrapping <p> tag)
-    print strip_tags($matches[1]); // Content of the first paragraph without the HTML tags.
+	setWiki($_GET["coinValue"]);
 }
 
